99e9dbab0a00830e2e485ce76229af81c36fc0ba
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-09-21 23:08:36 -0600

    added feedback notes

--- objects-lab.js
+++ objects-lab.js
@@ -1,3 +1,9 @@
+//I would recommend trying out letting the user pick a range instead of hard coding biggestNum and smallestNum, it is a good challenge.
+
+//One thing you might want to solve is getting the cancel button to close the prompt window
+//cancel === NaN
+//I added code to do so, no perfect but working.. 
+
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
@@ -5,26 +11,30 @@ const game = {
   secretNum: null,
   prevGuesses: [],
   play: function () {
-      this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-        let guess = NaN;
-
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    //initialized guess to nothing so that I can hit prompt's cancel
+        let guess
           while(guess !== this.secretNum) {
             guess = this.getGuess();
-            this.prevGuesses.push(guess);
+            //below so that NaN doesn't show up in prevGuesses - still prompts too low when NaN
+            if(guess)this.prevGuesses.push(guess);
             this.render(guess);
-            if(guess === this.secretNum) return;
+            if (guess === this.secretNum) return;
+            // return if cancel button selected
+            if(!guess)return
           }
         },
-  getGuess: function(){
-    let guess = NaN; 
-    while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
-      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `))
+  getGuess: function(){ 
+    let guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `))
+    //removed while loop so that I can use the cancel the prompt in play() added alert per if()
+    if (guess > this.biggestNum || guess < this.smallestNum) {
+      alert(`${guess} is not within the guessing range`)
     }
-    return guess;  //add guess to PrevGuesses
+      return guess;  //add guess to PrevGuesses
                     // return the guess value
   },
 
-
+//Nice job using the ternary operator! It is good practice
 render: function(guess) {
   let msg = (guess === this.secretNum) ?
     `Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`: // Congrats! You guessed the number in [x] guesses!
